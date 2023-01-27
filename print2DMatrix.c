c4405d91c1c3146d389200336ffdee501727df3f
gokhansahinn <gkhnsahin4848@gmail.com>
2023-01-27 17:52:07 +0300

    There is a segmentation fault

---
+++ print2DMatrix.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Create Matrix
+//Assign Values
+//Print Matrix
+void createMatrix(int **newArray, int* rows,int* columnsArray)
+{
+    for(int i=0; i<*rows ; i++)
+    {
+        printf("Enter a column number for %d. rows: ",i+1);
+        scanf("%d",&columnsArray[i]);
+        newArray[i]= (int *)malloc(columnsArray[i]*sizeof(int));
+        for(int j=0; j<columnsArray[i] ; j++)
+        {
+            printf("newarray[%d][%d] = %d\n",i,j,newArray[i][j]);  
+        }
+    }
+}
+void assignValuesMatrix(int **array,int rows, int* columnsArray)
+{
+    int value;
+    for(int i = 0; i<rows ; i++)
+    {   
+        for(int j=0; j<columnsArray[i] ; j++)
+        {
+            printf("array[%d][%d] = %d\n",i,j,array[i][j]);  
+        }
+        printf("\n");
+    }
+    for(int i = 0; i<rows ; i++)
+    {
+        for(int j=0; j<columnsArray[i] ; j++)
+        {
+            printf("Enter value for array[%d][%d]: ",i,j);
+            scanf("%d",&value);
+            array[i][j]=value;
+        }
+
+    }
+
+}
+void printValuesMatrix(int **array,int rows, int* columnsArray)
+{
+    for(int i = 0; i<rows ; i++)
+    {   
+        for(int j=0; j<columnsArray[i] ; j++)
+        {
+            printf("array[%d][%d] = %d\n",i,j,array[i][j]);  
+        }
+        printf("\n");
+    }
+
+}
+int main()
+{
+    int ** myMatrix;
+    int * columnsArray;
+    int rows;
+    printf("enter a row number for 2D Matrix: ");
+    scanf("%d",&rows);
+    columnsArray = (int* )calloc(rows,sizeof(int));
+    myMatrix = (int **)calloc(rows, sizeof(int));
+    createMatrix(myMatrix, &rows,columnsArray);
+    for(int i = 0; i<rows ; i++)
+    {   
+        for(int j=0; j<columnsArray[i] ; j++)
+        {
+            printf("myMatrix[%d][%d] = %d\n",i,j,myMatrix[i][j]);  
+        }
+        printf("\n");
+    }
+    for(int i=0 ; i<rows;i++)
+    {
+        printf("columnsArray[%d]=%d\n",i,columnsArray[i]);
+    }
+    printf("\n");
+    assignValuesMatrix(myMatrix,rows,columnsArray);
+    printValuesMatrix(myMatrix,rows,columnsArray);
+    
+    return 0;
+}
