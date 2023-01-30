a963506962dcf149b480e23f3738f23d62b78ed5
gokhansahinn <gkhnsahin4848@gmail.com>
2023-01-30 14:47:42 +0300

    in this function (int **)calloc(rows, sizeof(int *)) fixed

--- 2DArrayStaticVsPointer.c
+++ 2DArrayStaticVsPointer.c
@@ -9,14 +9,21 @@ int main()
     int rows,coulums;
     printf("Enter row number for 2D array: ");
     scanf("%d",&rows);
-    a=(int **)calloc(rows,sizeof(int));
+    a=(int **)calloc(rows,sizeof(int *));
     for(int i=0 ; i<rows; i++)
     {
         printf("Enter coulums number %d .rows for 2D array: ",i+1);
         scanf("%d",&coulums);
         a[i]=(int *)calloc(coulums,sizeof(int));
     }
-    a[1][2]=20;
-    printf("a[1][2] = %d\n",a[1][2]);
+    for(int i = 0; i<rows ; i++)
+    {
+        for(int j = 0 ; j< coulums ; j++)
+        {
+            printf("Enter a value a[%d][%d]: ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("a[4][0] = %d\n",a[4][0]);
     return 0;
 }
